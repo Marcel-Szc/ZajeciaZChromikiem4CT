86e222af59a7c235568ca2e7bb69a49185a1706d
Kamil6747 <kamlbaron3523@gmail.com>
2025-10-16 14:47:44 +0200

    16.10.2025v2

---
+++ 16.10.2025/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Kontener
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MojaKlasa mojaKlasa = new();
+            mojaKlasa.MojaMetoda();
+
+            Klasa4TC.MojaMetoda();
+
+        }
+    }
+    public class MojaKlasa
+    {
+        // klasie instancjonowanej możemy tworzyć metody zarówno statyczne jak i niestatyczne
+        public void MojaMetoda()
+        {
+
+        }
+        public static void inna()
+        {
+
+        }
+    }
+    public static class Klasa4TC
+    {
+        // klasy statycznej nie możemy instancjonować
+        // W klasie statycznej możemy tworzyć tylko i wyłącznie metody statyczne
+        public static void MojaMetoda()
+        {
+
+        }
+    }
+}
---
+++ 16.10.2025/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,101 @@
+﻿
+using System.Collections.ObjectModel;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Zegar zegar = new();
+        zegar.Konfiguruj("Alaska", 1);
+
+        Zegar zegar1 = new();
+        zegar1.Konfiguruj("Hawaje", 2);
+
+        Console.ReadKey();
+    }
+}
+class Zegar
+{
+    private string nazwaStrefy;
+    private int numerLinii;
+
+    ReadOnlyCollection<TimeZoneInfo> tzi;
+    System.Timers.Timer timer;
+
+    public void Konfiguruj(string nazwaStrefy, int numerLinii)
+    {
+        this.nazwaStrefy = nazwaStrefy;
+        this.numerLinii = numerLinii;
+
+        System.Timers.Timer timer;
+        timer = new System.Timers.Timer();
+        timer.Interval = 1000;
+        timer.Elapsed += Timer_Elapsed;
+        timer.Start();
+    }
+
+    void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+    {
+        tzi = TimeZoneInfo.GetSystemTimeZones();
+        nazwaStrefy = "Alaska";
+        numerLinii = 0;
+        foreach (TimeZoneInfo timeZone in tzi)
+        {
+            if (timeZone.DisplayName.IndexOf(nazwaStrefy) > 0)
+            {
+                Console.SetCursorPosition(0, numerLinii);
+                Console.WriteLine($"\r {timeZone.DisplayName} {TimeZoneInfo.ConvertTimeBySystemTimeZoneId(
+                    DateTime.Now,
+                    TimeZoneInfo.Local.Id,
+                    timeZone.Id
+                )}");
+            }
+        }
+    }
+}
+/*
+class Zegar
+{
+    private string nazwaStrefy;
+    private int numerLinii;
+
+    ReadOnlyCollection<TimeZoneInfo> tzi;
+    System.Timers.Timer timer;
+
+    public void Config(string nazwaStrefy, int numerLinii) {
+        this.nazwaStrefy = nazwaStrefy;
+        this.numerLinii = numerLinii;
+
+        timer = new System.Timers.Timer();
+        timer.Interval = 1000;
+        timer.Elapsed += Timer_Elapsed;
+        timer.Start();
+    }
+    private void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+    {
+        tzi = TimeZoneInfo.GetSystemTimeZones();
+        foreach (TimeZoneInfo timeZone in tzi)
+        {
+            if (timeZone.DisplayName.IndexOf(this.nazwaStrefy) > 0)
+            {
+                Console.SetCursorPosition(0, numerLinii);
+                Console.WriteLine($"\r {timeZone.DisplayName} {TimeZoneInfo.ConvertTimeBySystemTimeZoneId(
+                    DateTime.Now,
+                    TimeZoneInfo.Local.Id,
+                    timeZone.Id
+                )}");
+            }
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Zegar zegar = new();
+        zegar.Config("Alaska", 0);
+        Zegar zegar1 = new();
+        zegar1.Config("Hawaje", 1);
+
+    }
+} */
