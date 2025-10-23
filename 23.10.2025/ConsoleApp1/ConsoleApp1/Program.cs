18e15e449714596b2119571de29354997c944347
Kamil6747 <kamlbaron3523@gmail.com>
2025-10-23 14:35:04 +0200

    23.10.2025v1

---
+++ 23.10.2025/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace ProgramStart
+{
+    using Figury;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zaimportowany kontener poprzez using
+            Kwadrat kwadrat = new Kwadrat();
+            kwadrat.bok = 12;
+            kwadrat.obwod = 10;
+            Console.WriteLine(kwadrat.obwod);
+            kwadrat.obwod = 0;
+            Console.ReadLine();
+            
+            //Bez importowania kontenera
+            Bryly.Szescian szescian = new Bryly.Szescian();
+        }
+    }
+}
+namespace Bryly
+{
+    class Szescian
+    {
+
+    }
+}
+namespace Figury
+{
+    class Kwadrat
+    {
+        public int bok; //ten sposób jest najmniej zalecany
+
+        public int obwod { 
+            get {
+                return _obwod; 
+            } 
+            set { 
+                if(value < 0)
+                {
+                    _obwod = value * -1;
+                } else if(value == 0){
+                    throw new ArgumentException("Nie może być zerem");
+                }
+                else
+                {
+                    _obwod = value;
+                } 
+            } 
+        }
+
+        private int _obwod;
+        public void ObliczPole(int a)
+        {
+            
+        }
+    }
+}
---
+++ 23.10.2025/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Start
+{
+    using Rachunkowosc;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj wartość netto: ");
+            double netto = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Podaj procent podatku: ");
+            double procent = Convert.ToDouble(Console.ReadLine());
+
+            Podatek podatek = new Podatek();
+            try
+            {
+                podatek.wartoszc_podatku = procent;
+                podatek.wartosc_netto = netto;
+
+                Console.WriteLine($"Wartość brutto: {podatek.ObliczPodatek()}");
+            }
+            catch (Exception ex) 
+            { 
+                Console.WriteLine($"Wystąpił błąd:  {ex.Message}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+namespace Rachunkowosc
+{
+    class Podatek
+    {
+
+        public double wartoszc_podatku {
+             get { return _podatek; }
+             set {
+                 if( value > 0)
+                 {
+                     _podatek = value;
+                 } else
+                 {
+                     _podatek = value * -1;
+                 }
+                 
+             }
+        }
+        private double _podatek;
+
+        public double wartosc_netto
+        {
+            get { return _podatek; }
+            set
+            {
+                if (value > 0)
+                {
+                    _wartosc_netto = value;
+                }
+                else if (value < 0)
+                {
+                    _wartosc_netto = value * -1;
+                }
+                else
+                {
+                    throw new ArgumentException("Wartość netto nie może być 0!");
+                }
+            }
+        }
+        private double _wartosc_netto;
+        public double ObliczPodatek()
+        {
+            double brutto = _wartosc_netto + ((_wartosc_netto / 100) * _podatek);
+
+            return brutto;
+        }
+    }
+}
